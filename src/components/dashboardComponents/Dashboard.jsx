6db1a823b12dfbb04315ba2488480d7fee101947
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-11-30 18:50:42 +0530

    added dashboard data

--- src/components/dashboardComponents/Dashboard.jsx
+++ src/components/dashboardComponents/Dashboard.jsx
@@ -11,17 +11,34 @@ import { getUserStats } from '../../services/api.service';
 
 
 export default function Dashboard() {
-  
-  let [data,setData] = useState('')
 
-useEffect(()=>{
-  getUserStats().then((res)=>{
-    console.log(res)
-  }).catch((err)=>{
-    console.log(err)
-  })
-},[])
-  
+  let [data, setData] = useState(
+    {
+      "username": "John Doe",
+      "email": "Johndoe@gmail.com",
+      "referalEmail": "N/A",
+      "totalDeposits": 0,
+      "referalLink": "http://localhost:5173/register/DEMO-TRUSTBOT-91",
+      "status": "unverified",
+      "profitRate": "0.73",
+      "tradeProfitIncome": 0,
+      "referalIncome": 0,
+      "totalBalance": 0,
+      "totalWithdrawals": 0,
+      "withdrawalBalance": 0
+  }
+  )
+
+  useEffect(() => {
+    getUserStats().then((res) => {
+      console.log(res)
+
+      setData(res.data.stats)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
 
 
 
@@ -32,8 +49,8 @@ useEffect(()=>{
 
 
         <div>
-            <p className='text-xl font-bold'>Status: <span className='text-red-700 text-xl'>Unverified</span></p>
-        </div> 
+          <p className='text-xl font-bold'>Status: <span className={data.status == "verified" ? "text-green-700 text-xl" : "text-red-700 text-xl"}>{data.status}</span></p>
+        </div>
 
 
         {/* <div className='w-full h-[80px] border rounded-lg shadow-lg shadow-cyan-500/50 flex items-center px-5'>
@@ -49,128 +66,127 @@ useEffect(()=>{
 
 
         <div className=" flex flex-col justify-center items-center p-4">
-      {/* Card Container */}
-      <div className="grid gap-6 md:grid-cols-2 w-full">
-        
-        {/* First Card */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex">
-          <div className='w-[40%] text:xl md:text-xl font-semibold'> 
-            <p>User Name</p>
-            <p>User Email</p>
-            <p>Referral Email</p>
-            
-          </div>
-          <div className='w-1/2 text:xl md:text-xl font-semibold'> 
-            <p>: User Name</p>
-            <p>: User Id</p>
-            <p>: Referral Id</p>
-            
-          </div>
-        </div>
+          {/* Card Container */}
+          <div className="grid gap-6 md:grid-cols-2 w-full">
 
-        {/* Second Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105">
-          <h2 className="text-md font-bold ">Referral Program  :  Earn a stable income by</h2>
-          <p className="text-lg font-bold">
-          introducing clients to Enter Company Name
-          </p>
-          <div className='w-full h-10  bg-gray-200 rounded-full flex items-center text-black text-xl font-bold justify-between overflow-hidden border'>
-                <p className='pl-5'>https://www.google.com</p>
+            {/* First Card */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex">
+              <div className='w-[40%] text:xl md:text-xl font-semibold'>
+                <p>User Name</p>
+                <p>User Email</p>
+                <p>Referral Email</p>
+              </div>
+              
+              <div className='w-1/2 text:xl md:text-xl font-semibold'>
+                <p>:{data?.username}</p>
+                <p>: {data?.email}</p>
+                <p>: {data.referalEmail}</p>
+              </div>
+            </div>
+
+            {/* Second Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105">
+              <h2 className="text-md font-bold ">Referral Program  :  Earn a stable income by</h2>
+              <p className="text-lg font-bold">
+                introducing clients to Enter Company Name
+              </p>
+              <div className='w-full h-10  bg-gray-200 rounded-full flex items-center text-black text-xl font-bold justify-between overflow-hidden border'>
+                <p className='pl-5'>{data.referalLink}</p>
                 <p className='bg-black p-5 rounded-full text-white cursor-pointer'>Copy</p>
+              </div>
+            </div>
+
           </div>
         </div>
-
-      </div>
-    </div>
 
 
 
         <div className=" flex flex-col justify-center items-center p-4">
-      {/* Card Container */}
-      <div className="grid gap-6 md:grid-cols-4 w-full">
-        
-        {/* First Card */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 justify-center  items-center">
-          <div className='text:xl font-semibold '> 
-            <p>Deposit </p>
-            <p className='text-center'>$ {data.selfDeposit}</p>
-          </div>
-          <div className='text:xl font-semibold'> 
-          <FcMoneyTransfer  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
+          {/* Card Container */}
+          <div className="grid gap-6 md:grid-cols-4 w-full">
 
-        {/* Second Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p>Daily Profit Rate </p>
-            <p className='text-center'>0.73 %</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <GiTakeMyMoney  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
+            {/* First Card */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 justify-center  items-center">
+              <div className='text:xl font-semibold '>
+                <p>Deposit </p>
+                <p className='text-center'>$ {data.totalDeposits}</p>
+              </div>
+              <div className='text:xl font-semibold'>
+                <FcMoneyTransfer className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
 
-        {/* Third Card with Search Field */}
+            {/* Second Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p>Daily Profit Rate </p>
+                <p className='text-center'>{data.profitRate}%</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <GiTakeMyMoney className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
 
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p>Trade Profit Income</p>
-            <p className='text-center'>$ {data.referralIncome}</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <GiReceiveMoney  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
+            {/* Third Card with Search Field */}
+
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p>Trade Profit Income</p>
+                <p className='text-center'>$ {data.tradeProfitIncome}</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <GiReceiveMoney className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
 
 
-        {/* Fourth Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p>Referral Income</p>
-            <p className='text-center'>$ {data.royaltyIncome}</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <RiMoneyDollarCircleFill  className='inline text-2xl md:text-4xl'/>
+            {/* Fourth Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p>Referral Income</p>
+                <p className='text-center'>$ {data.referalIncome}</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <RiMoneyDollarCircleFill className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
+
+            {/* Five Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p>Total Balance </p>
+                <p className='text-center'>$ {data.totalBalance}</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <BsCashCoin className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
+
+            {/* Six Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p>Total Withdrawal</p>
+                <p className='text-center'>$ {data.totalWithdrawals}</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <FaAward className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
+
+            {/* Seven Card with Search Field */}
+            <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
+              <div className='text:xl  font-semibold'>
+                <p className='text-center'>Withdrawable Balance </p>
+                <p className='text-center'>$ {data.withdrawalBalance}</p>
+              </div>
+              <div className=' text:xl font-semibold'>
+                <GiMoneyStack className='inline text-2xl md:text-4xl' />
+              </div>
+            </div>
+
+
           </div>
         </div>
-       
-        {/* Five Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p>Total Balance </p>
-            <p className='text-center'>$ {data.selfDepositCharges}</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <BsCashCoin  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
-       
-        {/* Six Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p>Total Withdrawal</p>
-            <p className='text-center'>$ {data.rewardIncome}</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <FaAward  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
-       
-        {/* Seven Card with Search Field */}
-        <div className="bg-black border p-6 rounded-lg shadow-lg hover:shadow-xl transform transition duration-300 hover:scale-105 flex flex-col gap-2 items-center">
-          <div className='text:xl  font-semibold'> 
-            <p className='text-center'>Withdrawable Balance </p>
-            <p className='text-center'>$ {data.totalIncome}</p>
-          </div>
-          <div className=' text:xl font-semibold'> 
-          <GiMoneyStack  className='inline text-2xl md:text-4xl'/>
-          </div>
-        </div>
-       
-               
-      </div>
-    </div>
       </div>
     </>
   )
--- src/components/dashboardComponents/Dashboard_SideBar.jsx
+++ src/components/dashboardComponents/Dashboard_SideBar.jsx
@@ -9,6 +9,7 @@ import { FaWallet } from "react-icons/fa";
 import { BsBank2 } from "react-icons/bs";
 import { RiLogoutCircleRLine } from "react-icons/ri";
 import logo from '../../assets/trustt.png'
+import {toast} from "react-toastify"
 
 
 
@@ -65,7 +66,10 @@ export default function Dashboard_SideBar() {
 
 
                 {/* Signuot */}
-                <li><Link to='/login' onClick={()=>{localStorage.removeItem("token")}} className='block pl-5 p-2 hover:bg-gray-700 active:bg-slate-950 ease-linear  border-b border-b-gray-500 text-xl font-semibold'><RiLogoutCircleRLine  className='inline text-2xl' /> &nbsp;&nbsp;Signout</Link></li>
+                <li><Link to='/login' onClick={()=>{
+                    localStorage.removeItem("token");
+                    toast.success("User logged out")
+                }} className='block pl-5 p-2 hover:bg-gray-700 active:bg-slate-950 ease-linear  border-b border-b-gray-500 text-xl font-semibold'><RiLogoutCircleRLine  className='inline text-2xl' /> &nbsp;&nbsp;Signout</Link></li>
             </ul>
         </div>
     </div>
